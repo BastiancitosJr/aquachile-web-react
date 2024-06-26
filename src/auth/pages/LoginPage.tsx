6bf82ba56da3e2025690cbaef3dbbab31ffa770d
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-26 01:49:14 -0400

    refactor: remove unused divs

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,19 +1,16 @@
 import { LoginForm } from "../components/LoginForm";
-import logo from "../../assets/img/AquaChileLOGO.png";
+import logo from "../../assets/img/aquachile-logo.webp";
 
 const LoginPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-          <div className="flex flex-col items-center w-full max-w-2xl">
-              <div className="w-full max-w-md bg-white shadow-2xl rounded-lg px-10 pt-8 pb-8 md:text-lg md:py-4 md:px-8 border-2 border-gray-200">
-                  <div className="flex justify-center items-center text-center">
-                      <img src={logo} alt="Logo de Aquachile" className="h-13" />
-                  </div> 
-                  <h1 className="text-center text-4xl py-10 text-blue-900"> Inicio de Sesión</h1>
-                  <LoginForm/>
-              </div>
-
-          </div>
+    <div className="w-full h-dvh flex justify-center items-center">
+      <div className="w-full max-w-lg bg-white shadow-2xl rounded-lg px-10 pt-8 pb-8 border-2 border-gray-200">
+        <img src={logo} alt="Logo de Aquachile" />
+        <h1 className="text-4xl lg:text-5xl text-aqcl-500 mt-3">
+          Inicio de Sesión
+        </h1>
+        <LoginForm />
+      </div>
     </div>
   );
 };
