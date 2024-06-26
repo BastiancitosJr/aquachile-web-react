4ce5f69bdb2e905c98c916e76681d07cb015ca2b
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-26 02:01:27 -0400

    refactor: change input and labels for flowbite components

--- src/auth/components/LoginForm.tsx
+++ src/auth/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ShowPasswordButton } from "../../common/components/ShowPasswordButton";
-import { Button } from "flowbite-react";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export function LoginForm() {
   const credentialsPlaceholder = "Credenciales";
@@ -17,58 +17,73 @@ export function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <div className="relative z-0 w-full mb-10 group">
-        <input
-          type="text"
-          name="username"
-          id="username"
-          autoComplete="username"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=""
-        />
-        <label
-          htmlFor="username"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          {credentialsPlaceholder}
-        </label>
+    <form
+      className="flex flex-col w-full h-full justify-start items-start mt-5"
+      onSubmit={handleLogin}
+    >
+      <div className="block w-full text-start mb-1">
+        <Label htmlFor="username-input" value="Tu Identificador" />
       </div>
-      <div className="relative z-0 w-full mb-10 group">
-        <input
-          type={showPassword ? "text" : "password"}
-          name="password"
-          id="password"
-          autoComplete="current-password"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=""
-        />
-        <label
-          htmlFor="password"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          {passwordPlaceholder}
-        </label>
-        <button
-          type="button"
-          onClick={() => setShowPassword(!showPassword)}
-          className="absolute right-0 top-3 text-sm text-blue-700 focus:outline-none"
-        >
-          <ShowPasswordButton showPassword={showPassword} />
-        </button>
+      <TextInput
+        className="w-full"
+        id="username-input"
+        type="text"
+        placeholder="Ej: Linea-1 o 12.123.123-K"
+      />
+
+      <div className="block w-full text-start mt-2 mb-1">
+        <Label htmlFor="password-input" value="Tu Identificador" />
       </div>
-      <div className="flex items-center justify-center py-5">
-        {/* <LoginButton /> */}
-        <Button
-          type="submit"
-          size="lg"
-          isProcessing={isProcessing}
-          className="login-button"
-          pill
-        >
-          Iniciar sesión
-        </Button>
-      </div>
+      <TextInput
+        className="w-full"
+        id="password-input"
+        type="password"
+        placeholder="******"
+      />
+      {/* <input
+        type="text"
+        name="username"
+        id="username"
+        autoComplete="username"
+        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=""
+      />
+      <label
+        htmlFor="username"
+        className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        {credentialsPlaceholder}
+      </label>
+      <input
+        type={showPassword ? "text" : "password"}
+        name="password"
+        id="password"
+        autoComplete="current-password"
+        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=""
+      />
+      <label
+        htmlFor="password"
+        className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        {passwordPlaceholder}
+      </label>
+      <button
+        type="button"
+        onClick={() => setShowPassword(!showPassword)}
+        className="absolute right-0 top-3 text-sm text-blue-700 focus:outline-none"
+      >
+        <ShowPasswordButton showPassword={showPassword} />
+      </button> */}
+      <Button
+        className="w-full mt-5"
+        type="submit"
+        size="lg"
+        color="enterprise"
+        isProcessing={isProcessing}
+      >
+        Iniciar sesión
+      </Button>
     </form>
   );
 }
--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import logo from "../../assets/img/aquachile-logo.webp";
 const LoginPage = () => {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div className="w-full max-w-lg bg-white shadow-2xl rounded-lg px-10 pt-8 pb-8 border-2 border-gray-200">
+      <div className="w-full max-w-lg bg-white shadow-2xl rounded-lg py-8 px-5 md:px-10 border-2 border-gray-200">
         <img src={logo} alt="Logo de Aquachile" />
         <h1 className="text-4xl lg:text-5xl text-aqcl-500 mt-3">
           Inicio de Sesión
