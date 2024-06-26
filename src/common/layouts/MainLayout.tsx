19958cbafbfaac2fbe9cddd80d19f7272365cca4
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-26 18:51:30 -0400

    fix: add vertical padding to main layout

--- src/common/layouts/MainLayout.tsx
+++ src/common/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import useUserInformation from "../../auth/hooks/useUserInformation";
 
 const baseClassName =
-  "flex flex-col text-center md:px-10 w-full md:pt-10 bg-gray-100";
+  "flex flex-col text-center md:px-10 w-full md:pt-10 bg-gray-100 py-10";
 
 interface Props {
   children: React.ReactNode;
