c394a6bc2dccd620478619776dc4ef3072112473
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-26 00:48:31 -0400

    fix: update sizing and position of elements in not found page

--- src/common/pages/NotFoundPage.tsx
+++ src/common/pages/NotFoundPage.tsx
@@ -1,29 +1,33 @@
 import { Button } from "flowbite-react";
+import { homePath } from "../router/routes-paths";
+
+const notFoundTexts = {
+  code: "404",
+  title: "Oops... Algo salió mal",
+  description:
+    "No pudimos encontrar lo que buscabas. Encontrarás mucho que explorar en la página principal.",
+  button: "Volver a la página principal",
+};
 
 const NotFoundPage = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-aqcl-600 dark:text-aqcl-500">
-            404
-          </h1>
-          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-            Oops... Algo salió mal
-          </p>
-          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-            No pudimos encontrar lo que buscabas. Encontrarás mucho que explorar
-            en la página principal.{" "}
-          </p>
-          <a
-            href="/"
-            className="inline-flex text-white bg-aqcl-500 hover:bg-aqcl-800 focus:ring-4 focus:outline-none focus:ring-aqcl-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-aqcl-900 my-4"
-          >
-            Volver al Inicio
-          </a>
-          <Button color="enterprise">Hola</Button>
-        </div>
-      </div>
+    <section className="bg-white items-center w-full h-dvh flex flex-col justify-center px-5 text-center">
+      <h1 className="mb-4 tracking-tight font-extrabold text-8xl lg:text-9xl text-aqcl-600 dark:text-aqcl-500">
+        {notFoundTexts.code}
+      </h1>
+      <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+        {notFoundTexts.title}
+      </p>
+      <p className="mb-4 text-lg text-gray-500 text-wrap">
+        {notFoundTexts.description}
+      </p>
+      <Button
+        className="mx-auto w-full md:w-1/2 lg:w-1/3 xl:w-1/4"
+        color="enterprise"
+        href={homePath}
+      >
+        {notFoundTexts.button}
+      </Button>
     </section>
   );
 };
