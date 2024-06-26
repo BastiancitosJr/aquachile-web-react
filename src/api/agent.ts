8744b6a93678aa7b070b9b1c56b530bfa01f4154
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-26 17:30:42 -0400

    fix: change auth path to match api

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -1,40 +1,36 @@
 import axios, { AxiosResponse } from "axios";
-import { apiUrl } from "./config/env";
+import { apiUrl } from "./config/envs";
 import { AuthDto } from "../auth/dtos/auth-dto";
 
 axios.defaults.baseURL = apiUrl;
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
-axios.interceptors.request.use((config) => {
-  const tokenStrJson = localStorage.getItem("token") ?? "";
-  const tokenObj = JSON.parse(tokenStrJson);
-  const token = tokenObj?.state?.token as string;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+// axios.interceptors.request.use((config) => {
+//   const tokenStrJson = localStorage.getItem("token") ?? "";
+//   const tokenObj = JSON.parse(tokenStrJson);
+//   const token = tokenObj?.state?.token as string;
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
 
-  return config;
-});
+//   return config;
+// });
 
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
   get: (url: string, params?: URLSearchParams) =>
     axios.get(url, { params }).then(responseBody),
-  post: (url: string, body: unknown) =>
-    axios.post(url, body).then(responseBody),
+  post: (url: string, body: Object) => axios.post(url, body).then(responseBody),
 
-  put: (url: string, body: unknown) => axios.put(url, body).then(responseBody),
+  put: (url: string, body: Object) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
-  patch: (url: string, body: unknown) =>
+  patch: (url: string, body: Object) =>
     axios.patch(url, body).then(responseBody),
 };
 
 const Auth = {
-  login: (form: AuthDto) => {
-    console.log("form", form);
-    return requests.post("api/login", form);
-  },
+  login: (form: AuthDto) => requests.post("auth/login", form),
 };
 
 const agent = { Auth };
