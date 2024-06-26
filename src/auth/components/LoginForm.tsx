b8026d24f219c45d67a77068b977c4820a48ce6c
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-06-26 00:55:46 -0400

    feat: showpassword

--- src/auth/components/LoginForm.tsx
+++ src/auth/components/LoginForm.tsx
@@ -1,33 +1,56 @@
 import { LoginButton } from "./LoginButton";
-
+import { useState } from "react";
+import { ShowPasswordButton } from "../../common/components/ShowPasswordButton";
 
 export function LoginForm() {
+  const credentialsPlaceholder = "Credenciales";
+  const passwordPlaceholder = "Contraseña";
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <form>
       <div className="relative z-0 w-full mb-10 group">
-        <input 
-        type="text" 
-        name="username" 
-        id="username" 
-        autoComplete="username" 
-        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
-        <label 
-        htmlFor="username" 
-        className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Credenciales</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          autoComplete="username"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=""
+        />
+        <label
+          htmlFor="username"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          {credentialsPlaceholder}
+        </label>
       </div>
       <div className="relative z-0 w-full mb-10 group">
-        <input 
-        type="password"
-        name="password"
-        id="password" 
-        autoComplete="current-password" 
-        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
-        <label 
-        htmlFor="password"
-        className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Contraseña</label>
+        <input
+          type={showPassword ? "text" : "password"}
+          name="password"
+          id="password"
+          autoComplete="current-password"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=""
+        />
+        <label
+          htmlFor="password"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          {passwordPlaceholder}
+        </label>
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-0 top-3 text-sm text-blue-700 focus:outline-none"
+        >
+          <ShowPasswordButton showPassword={showPassword} />
+        </button>
       </div>
       <div className="flex items-center justify-center py-5">
-        <LoginButton/>
+        <LoginButton />
       </div>
     </form>
   );
