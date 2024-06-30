33dd779b2c022340773f954b4aa3fa422cbf402b
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-30 16:40:00 -0400

    fix: change type of axios body to unknown instead of Object

--- src/api/hooks/useAxios.tsx
+++ src/api/hooks/useAxios.tsx
@@ -30,12 +30,12 @@ const useAxios = () => {
   );
 
   const post = useCallback(
-    (url: string, body: Object) => axios.post(url, body).then(responseBody),
+    (url: string, body: unknown) => axios.post(url, body).then(responseBody),
     []
   );
 
   const put = useCallback(
-    (url: string, body: Object) => axios.put(url, body).then(responseBody),
+    (url: string, body: unknown) => axios.put(url, body).then(responseBody),
     []
   );
 
@@ -45,7 +45,7 @@ const useAxios = () => {
   );
 
   const patch = useCallback(
-    (url: string, body: Object) => axios.patch(url, body).then(responseBody),
+    (url: string, body: unknown) => axios.patch(url, body).then(responseBody),
     []
   );
 
