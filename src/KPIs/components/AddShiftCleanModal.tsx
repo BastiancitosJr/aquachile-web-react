9373ee1b9c7858943b215f554c7ad5478931f487
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-30 18:23:50 -0400

    feat: add validations to ShiftCleanModal

--- src/KPIs/components/AddShiftCleanModal.tsx
+++ src/KPIs/components/AddShiftCleanModal.tsx
@@ -1,11 +1,17 @@
 import { Button, Label, Modal, Radio, Textarea } from "flowbite-react";
 import { cleaningKPI } from "../../home/constants/kpi-data";
 import Divider from "../../common/components/Divider";
+import { useForm } from "react-hook-form";
 
 const formTexts = {
   title: cleaningKPI.title,
   button: "Enviar Revisión",
   isAuditOk: "¿El lugar de trabajo se encuentra limpio al recibirlo?",
+};
+
+type FormInputs = {
+  auditOptions: string;
+  auditComment: string;
 };
 
 interface Props {
@@ -14,10 +20,20 @@ interface Props {
 }
 
 const AddShiftCleanModal = ({ show, onModalClose }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
   if (!show) return null;
 
   const handleClose = () => {
     onModalClose();
+  };
+
+  const onSubmit = (data: FormInputs) => {
+    console.log(data);
   };
 
   return (
@@ -29,38 +45,75 @@ const AddShiftCleanModal = ({ show, onModalClose }: Props) => {
             {formTexts.title}
           </h3>
           <Divider className="my-5" />
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="w-full flex justify-between px-16 mt-5">
               <legend className="text-center my-5 text-xl text-aqcl-500 font-semibold">
                 {formTexts.isAuditOk}
               </legend>
               <div className="flex items-center gap-2">
-                <Radio id="no-option" name="audit-options" value="NO" />
                 <Label htmlFor="no-option" className="uppercase text-xl">
                   NO
                 </Label>
+                <Radio
+                  id="no-option"
+                  {...register("auditOptions", {
+                    required: {
+                      value: true,
+                      message: "Debes seleccionar una opción",
+                    },
+                  })}
+                  name="audit-options"
+                  value="NO"
+                />
               </div>
               <div className="flex items-center gap-2">
-                <Radio
-                  id="yes-options"
-                  name="audit-options"
-                  value="SI"
-                  defaultChecked
-                />
                 <Label htmlFor="yes-options" className="uppercase text-xl">
                   SI
                 </Label>
+                <Radio
+                  id="yes-options"
+                  {...register("auditOptions", {
+                    required: {
+                      value: true,
+                      message: "Debes seleccionar una opción",
+                    },
+                  })}
+                  name="audit-options"
+                  value="SI"
+                />
               </div>
             </fieldset>
+            {errors.auditOptions && (
+              <p className="text-center mt-3 text-red-500">
+                {errors.auditOptions?.message}
+              </p>
+            )}
             <div className="mb-2 block mt-10">
               <Label htmlFor="audit-comment" value="Comentario o Apreciación" />
             </div>
             <Textarea
+              {...register("auditComment", {
+                required: {
+                  value: true,
+                  message: "Debes ingresar un comentario",
+                },
+                minLength: {
+                  value: 5,
+                  message: "El comentario debe tener al menos 5 caracteres",
+                },
+                maxLength: {
+                  value: 200,
+                  message: "El comentario no puede tener más de 200 caracteres",
+                },
+              })}
               rows={4}
               id="audit-comment"
               placeholder="Ej: El lugar de trabajo estaba limpio y ordenado..."
-              color="enterprise"
+              color={errors.auditComment?.message ? "failure" : "enterprise"}
             />
+            {errors.auditComment && (
+              <p className="text-red-500">{errors.auditComment?.message}</p>
+            )}
             <Button className="w-full mt-5" type="submit" color="enterprise">
               {formTexts.button}
             </Button>
