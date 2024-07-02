9e6ec93ff80c5ac02d5f14f45e5427bbfa4df84b
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-07-02 02:28:21 -0400

    feat: useCreateNew variable asigned

--- src/KPIs/hooks/quality/useCreateNewAudit.tsx
+++ src/KPIs/hooks/quality/useCreateNewAudit.tsx
@@ -1,2 +1,16 @@
 import useAxios from "../../../api/hooks/useAxios";
 import { CreateQualityDto } from "../../dtos/quality/create-quality-dto";
+import { CreateQualityToCreateCleaningAPIDto } from "../../../home/services/home-mapper";
+
+const useCreateNewAudit = () => {
+  const { post } = useAxios();
+
+  const createNew = async (lineId: string, form: CreateQualityDto) => {
+    const mappedBody = CreateQualityToCreateCleaningAPIDto(form);
+    const response = await post(`labeling-qualities/${lineId}`, mappedBody);
+  };
+
+  return createNew;
+};
+
+export default useCreateNewAudit;
