0c655b0d40c3f85458c9b7011b098e177ac17230
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-07-03 07:43:56 -0400

    fix: added correct function

--- src/KPIs/hooks/incidents/useCreateNewIncidents.tsx
+++ src/KPIs/hooks/incidents/useCreateNewIncidents.tsx
@@ -1,11 +1,13 @@
 import useAxios from "../../../api/hooks/useAxios";
+import { CreateIncidentsDtoToCreateIncidentsAPIDto } from "../../../home/services/home-mapper";
+import { CreateIncidentsDto } from "../../dtos/incidents/create-incidents-dto";
 
 const useCreateNewIncidents = () => {
   const { post } = useAxios();
 
   const createNew = async (lineId: string, form: CreateIncidentsDto) => {
     const mappedBody = CreateIncidentsDtoToCreateIncidentsAPIDto(form);
-    await post(`incidents/${lineId}`, mappedBody);
+    await post(`securities/${lineId}`, mappedBody);
   };
 
   return createNew;
