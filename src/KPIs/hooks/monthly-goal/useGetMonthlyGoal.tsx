10f5f52a3b06b89b32c344e6d625a7a0baad1b4e
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-07-03 11:54:27 -0400

    fix: change endpoint to get monthly goal

--- src/KPIs/hooks/monthly-goal/useGetMonthlyGoal.tsx
+++ src/KPIs/hooks/monthly-goal/useGetMonthlyGoal.tsx
@@ -1,17 +1,13 @@
 import useAxios from "../../../api/hooks/useAxios";
-import useUserInformation from "../../../auth/hooks/useUserInformation";
 import { MonthlyGoalResponseDto } from "../../dtos/monthly-goal/get-monthly-goal-response-dto";
 import { MonthlyGoal } from "../../models/monthly-goal/monthly-goal-response";
 import { mapMonthlyGoalResponseDtoToMonthlyGoal } from "../../services/kpis-mapper";
 
 const useGetMonthlyGoal = () => {
   const { get } = useAxios();
-  const { shiftId } = useUserInformation();
   const getCleaningAudit = async (): Promise<MonthlyGoal | undefined> => {
     try {
-      const response: MonthlyGoalResponseDto[] = await get(
-        `monthly-pps-shift/${shiftId}`
-      );
+      const response: MonthlyGoalResponseDto[] = await get("monthly-pps");
       const mappedData = mapMonthlyGoalResponseDtoToMonthlyGoal(
         response[response.length - 1]
       );
